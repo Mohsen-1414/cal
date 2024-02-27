3e4a91c4d4df3d7575d3bc993e874f9afeef50f1
Mohsen <sm.shafaei@gmail.com>
2024-02-27 18:14:11 +0300

    version2

---
+++ interaction.js
@@ -0,0 +1,42 @@
+function onClick() {
+    if (emptyInput()) {
+      label.textContent = 'Error: one or both inputs are empty.';
+      return;
+    }
+    updateLabel();
+  }
+  function emptyInput() {
+    if (getNumber1() === '' || getNumber2() === '') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  
+  function updateLabel() {
+    var digit1 = getNumber1();
+    var digit2 = getNumber2();
+    if(button.value === 'Add'){
+      var sum = parseInt(digit1) + parseInt(digit2);
+      label.textContent = `${digit1} + ${digit2}  =  ${sum}`;
+    }
+    else{
+        var sum = parseInt(digit1) - parseInt(digit2);
+        label.textContent = `${digit1} - ${digit2}  =  ${sum}`;
+  }
+  }
+  
+  function getNumber1() {
+    return inputs[0].value;
+  }
+  function getNumber2() {
+    return inputs[1].value;
+  }
+
+
+  let inputs = document.querySelectorAll('input');
+  let label = document.querySelector('p');
+  let button = document.querySelector('button');
+  button.addEventListener('click', onClick);
+
